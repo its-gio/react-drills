952115cdcc667a36cc7ad4b31d703fe6a88c21c8
Giovani Aguirre <me@its-g.io>
2020-01-20 15:32:05 -0600

    Finished createing list of h2s and adding to DOM

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      list: ["This", "Is", "A", "List"]
+    }
+  }
+
+  render() {
+    let h2 = this.state.list.map((item, i) => <h2 key={i}>{item}</h2>)
+
+    return (
+      <div className="App">
+        {h2}
+      </div>
+    );
+  }
 }
 
 export default App;
