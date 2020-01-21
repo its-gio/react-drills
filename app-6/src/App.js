5b7fcb33340188c1facb34dd65688b658bd9a447
Giovani Aguirre <me@its-g.io>
2020-01-21 09:24:39 -0600

    Simple list add complete

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from "./component/List.jsx"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userInput: '',
+      list: []
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleInput(e) {
+    this.setState({ userInput: e.target.value })
+  }
+
+  handleAdd() {
+    this.setState(currState => {
+      let list = currState.list.concat(currState.userInput);
+      return {
+        list,
+        userInput: ''
+      }
+    })
+  }
+
+  render() {
+    let para = this.state.list.map((item, i) => <List key={i} list={item} />)
+    return (
+      <div className="App">
+        <h1>Todo List</h1>
+        <input onChange={this.handleInput} value={this.state.userInput} type="text" />
+        <button onClick={this.handleAdd}>Add</button>
+        { para }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ app-6/src/component/List.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function List(props) {
+  return (
+    <div>
+      <p>{props.list}</p>
+    </div>
+  )
+}
