13cb01f480a37174ddba87fa8002805b8a312653
Giovani Aguirre <me@its-g.io>
2020-01-20 23:40:18 -0600

    Passed props to image component

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ImageComp from './components/ImageComp.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      url: "http://via.placeholder.com/150"
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <ImageComp url={this.state.url} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ app-5/src/components/ImageComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ImageComp(props) {
+  return (
+    <img src={props.url} />
+  )
+}
+
+export default ImageComp
